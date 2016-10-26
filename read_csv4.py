828bb0171db6e25e6a0a57721dfc63bce495a5be
Shawn Conrad <Shawn Conrad>
2016-10-26 11:06:42 -0500

    Added looping - need logic.

--- read_csv4.py
+++ read_csv4.py
@@ -1,24 +1,31 @@
 import csv
 import sys
 
-
-if len(sys.argv) < 3:
-    print("++++++++ SERVICE TAG FINDER ++++++++")
-    print("Please enter the name of the person.")
-    print("++++++++++++++++++++++++++++++++++++")
-    search_1 = input("Search for:")
-else:
-    search_1 = sys.argv[1]
+start_again = True
 
 
-csvfile = open('ServiceTag.csv', 'rt', encoding="Latin-1")
-csvfile.seek
-reader = csv.reader(csvfile, dialect='excel', delimiter=",", quotechar="'")
+def start():
+    if len(sys.argv) < 2:
+        print("                                     ")
+        print("-+-+-+-+++++    SERVICE TAG FINDER     +++++-+-+-+-+")
+        print(" Enter the first name of the person you are searching for.")
+        print(" Press 'Enter' to view list. Press 'Ctrl + 'C' to exit.")
+        search_1 = input(" Search for:")
+    else:
+        search_1 = sys.argv[1]
 
-for row in reader:
-    if any(search_1 in col for col in row):
-        print("Results = " + ' '.join(row))
-        break
+    csvfile = open('ServiceTag.csv', 'rt', encoding="Latin-1")
+    csvfile.seek
+    reader = csv.reader(csvfile, dialect='excel', delimiter=",")
 
-else:
-    print("No results found for " + search_1)
+    for row in reader:
+        if any(search_1 in col for col in row):
+            print("Results = " + ' - '.join(row))
+    # TODO: Problem with returning print statement below even when results are
+    # found.
+    else:
+        print("Results not found for: " + "'" + search_1 + "'")
+
+
+while start_again:
+    start()
